511dfb59ec1576d27b4f03dcaad291f47cbff45a
jonslh <31646982+jonslh@users.noreply.github.com>
2018-02-16 10:35:57 +0100

    15.02.18

--- Inc/motorDriver.h
+++ Inc/motorDriver.h
@@ -1,15 +1,31 @@
-/*
- * motorDriver.h
- *
- *  Created on: 14. feb. 2018
- *      Author: haako
- */
 
 #ifndef MOTORDRIVER_H_
 #define MOTORDRIVER_H_
 
 #include "stm32f4xx_hal.h"
 #include "main.h"
+
+
+#define MOTOR_FRAM()   	{	HAL_TIM_PWM_Stop(&htim4,TIM_CHANNEL_1);									\
+							HAL_Delay(1);															\
+							HAL_GPIO_WritePin(DRIVE_DIR_GPIO_Port, DRIVE_DIR_Pin, GPIO_PIN_SET);	\
+							HAL_Delay(1);															\
+							HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_1);								}
+
+#define MOTOR_BAK()   	{	HAL_TIM_PWM_Stop(&htim4,TIM_CHANNEL_1);									\
+							HAL_Delay(1);															\
+							HAL_GPIO_WritePin(DRIVE_DIR_GPIO_Port, DRIVE_DIR_Pin, GPIO_PIN_RESET);	\
+							HAL_Delay(1);															\
+							HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_1);								}
+
+#define MOTOR_ENABLE()  {	HAL_GPIO_WritePin(DRIVE_EN_GPIO_Port, DRIVE_EN_Pin, GPIO_PIN_SET); 		\
+							HAL_Delay(1); 															\
+							HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_1);								}
+
+#define MOTOR_DISABLE()	{	HAL_TIM_PWM_Stop(&htim4,TIM_CHANNEL_1); 								\
+							HAL_Delay(1);															\
+							HAL_GPIO_WritePin(DRIVE_EN_GPIO_Port, DRIVE_EN_Pin, GPIO_PIN_RESET);	}
+
 
 void PWM_Set_Frekvens(uint16_t fart);
 
--- Src/motorDriver.c
+++ Src/motorDriver.c
@@ -1,13 +1,25 @@
 #include "motorDriver.h"
 #include "main.h"
 
-#define MOTOR_FRAM()   HAL_GPIO_WritePin(DRIVE_DIR_GPIO_Port, DRIVE_DIR_Pin, GPIO_PIN_SET)
-#define MOTOR_BAK()   HAL_GPIO_WritePin(DRIVE_DIR_GPIO_Port, DRIVE_DIR_Pin, GPIO_PIN_RESET)
-#define MOTOR_ENABLE()   {	HAL_GPIO_WritePin(DRIVE_EN_GPIO_Port, DRIVE_EN_Pin, GPIO_PIN_SET); \
-							HAL_Delay(10000); \
-							HAL_TIM_PWM_Start(&htim4,TIM_CHANNEL_1);\
-							HAL_Delay(5000);}
-#define MOTOR_DISABLE()   {	HAL_TIM_PWM_Stop(&htim4,TIM_CHANNEL_1); \
-							HAL_Delay(3000);\
-							HAL_GPIO_WritePin(DRIVE_EN_GPIO_Port, DRIVE_EN_Pin, GPIO_PIN_RESET); \
-							HAL_Delay(10000);}
+
+
+void PWM_Set_Frekvens(uint16_t tempfart){
+
+	bool retning = (tempfart>>15) & 0x01;
+	tempfart &= 0xEF;
+
+	if (retning==1) {
+		MOTOR_FRAM();
+	}
+	else{
+		MOTOR_BAK();
+	}
+
+	tempfart = 84000000 / tempfart * 6;
+
+
+	TIM4->ARR = 420;
+	TIM4->CCR1 = (420/2);
+	TIM4->CR1 = 0x81;
+
+}
